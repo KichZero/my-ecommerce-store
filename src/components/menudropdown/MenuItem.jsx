876807efecae4fb15c7aafa61f32aa86c24f6f5d
Max G <user@MacBook-Pro.local>
2024-11-12 19:16:58 +0200

    update2

--- src/components/menudropdown/MenuItem.jsx
+++ src/components/menudropdown/MenuItem.jsx
@@ -1,21 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 const MenuItem = ({ title, subCategories }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const timeoutRef = useRef(null);
+
+  const handleMouseEnter = () => {
+    if (timeoutRef.current) clearTimeout(timeoutRef.current);
+    setIsOpen(true);
+  };
+
+  const handleMouseLeave = () => {
+    timeoutRef.current = setTimeout(() => {
+      setIsOpen(false);
+    }, 50); // Устанавливаем задержку закрытия
+  };
 
   return (
     <div
       className="menu-item"
-      onMouseEnter={() => setIsOpen(true)}
-      onMouseLeave={() => setIsOpen(false)}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
     >
       <span className="menu-title">{title}</span>
       {isOpen && subCategories && subCategories.length > 0 && (
-        <div
-          className="dropdown-menu"
-          onMouseEnter={() => setIsOpen(true)} // Сохраняем меню открытым при наведении на него
-          onMouseLeave={() => setIsOpen(false)} // Закрываем меню при выходе курсора
-        >
+        <div className="dropdown-menu">
           {subCategories.map((category, index) => (
             <div key={index} className="dropdown-column">
               <span className="category-title">{category.category}</span>
